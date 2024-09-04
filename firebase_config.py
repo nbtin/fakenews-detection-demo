36458c1d4c368255b3deece4014c4a9948ce1daf
Nguyen Bao Tin <legendarystudent2402@gmail.com>
2024-09-04 08:05:37 +0700

    update configs

--- firebase_config.py
+++ firebase_config.py
@@ -1,16 +1,3 @@
-# For Firebase JS SDK v7.20.0 and later, measurementId is optional
-# firebaseConfig = {
-#   "apiKey": "AIzaSyAjfaU_gONrfSCU1Yl50Z40AECMWhZbub4",
-#   "authDomain": "fakenews-4048f.firebaseapp.com",
-#   "projectId": "fakenews-4048f",
-#   "storageBucket": "fakenews-4048f.appspot.com",
-#   "messagingSenderId": "888362903257",
-#   "appId": "1:888362903257:web:5d9cc77d9e5587597500c8",
-#   "measurementId": "G-PBXJW8LNXT",
-#   "serviceAccount": "service_account.json",
-#   "databaseURL": "https://fakenews-4048f-default-rtdb.firebaseio.com"
-# }
-
 from firebase_admin import credentials, initialize_app, storage
 
 # Init firebase with your credentials
